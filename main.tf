fec34509b02ce23c5b2aea3f850026e27750e728
cretu <crerem@gmail.com>
2023-03-14 15:04:20 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+
+
+
+    // use this when working with with Terraform cloud  
+    cloud {
+        organization = "curlycloud"
+    }
+
+    workspaces{
+        name = "multi-repo-root"
+    }
+
+
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "4.34.0"
+        }
+        github = {
+            source  = "integrations/github"
+            version = "~> 5.0"
+        }
+    }
+
+}
+
+// this is for terraform registy module 
+module "sample-module" {
+  source  = "app.terraform.io/curlycloud/sample-module/aws"
+  version = "1.1.0"
+}
+
+
+/*
+// This is for Gitub module
+
+provider "github" {
+  token = "Github Token"
+}
+
+
+module "testing" {
+  source = "github.com/crerem/multi-repo-terraform-module"
+}
+    
+*/
